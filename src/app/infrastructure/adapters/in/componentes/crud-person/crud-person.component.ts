d9ba00549d808be48a46c3e1102abd60bb826e5d
otakuSF <freddy.santacruzm@ucuenca.edu.ec>
2024-05-01 06:39:57 -0500

    Refactoring code, include in graphql.providers InPort and OutPort

--- src/app/infrastructure/adapters/in/componentes/crud-person/crud-person.component.ts
+++ src/app/infrastructure/adapters/in/componentes/crud-person/crud-person.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PersonInAdapter } from '../../../../../application/usesCases/PersonInAdapter';
-import { PersonInPort } from '../../../../../domain/ports/in/PersonInPort';
-import { PersonOutAdapter } from '../../../out/restApi/controllers/PersonOutAdapter';
-import { PersonOutPort } from '../../../../../domain/ports/out/personOutPort';
 @Component({
   selector: 'app-crud-person',
   standalone: true,
@@ -22,7 +19,4 @@ export class CrudPersonComponent implements OnInit {
   ngOnInit(): void {
     this.personInAdapter.getPerson(222)
   }
-
-
-
 }
